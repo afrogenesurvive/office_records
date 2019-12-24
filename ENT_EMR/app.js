5975248a69b6d9a47db99f05436fe8004d39556e
Michael Grandison <african.genetic.survival@gmail.com>
2019-12-24 14:29:31 -0500

    aws eb deploy test. db changed to atlas

--- ENT_EMR/app.js
+++ ENT_EMR/app.js
@@ -48,8 +48,8 @@ app.use(
 // });
 
 
-mongoose.connect('mongodb://localhost:27017/ent_emr_dev',{useNewUrlParser: true, useUnifiedTopology: true})
-// mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
+// mongoose.connect('mongodb://localhost:27017/ent_emr_dev',{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => {
     app.listen(10000);
   })
