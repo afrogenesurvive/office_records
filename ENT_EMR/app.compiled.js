08e19fe935481085adbe53f35ba465dc0b6df802
Michael Grandison <african.genetic.survival@gmail.com>
2019-12-24 13:38:31 -0500

    Build server and add .ebextensions

---
+++ ENT_EMR/app.compiled.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+
+const { pocketVariables } = require('./helpers/pocketVars');
+
+const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+const isAuth = require('./middleware/is-auth');
+const path = require('path');
+
+const multer = require('multer');
+const upload = multer({ dest: './uploads/' });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(isAuth);
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+app.use(isAuth);
+
+app.use('/graphql', graphqlHttp({
+  schema: graphQlSchema,
+  rootValue: graphQlResolvers,
+  graphiql: true
+}));
+
+// run react build***
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+
+mongoose.connect('mongodb://localhost:27017/ent_emr_dev', { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+  app.listen(10000);
+}).catch(err => {
+  console.log(err);
+});
